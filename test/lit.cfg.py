62f6614a595cd4392bf54aa4b0bfbbbca2251937
Roberth Kulbin <roberth@winter-weht.net>
2018-11-01 18:13:34 +0000

    Exclude lit.cfg.py from tests

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -5,6 +5,7 @@ import lit.formats
 
 config.name = 'llvmPy'
 config.suffixes = ['.py', '.ll']
+config.excludes = ['lit.cfg.py']
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = mkdtemp('llvmPy-lit-')
 config.test_format = lit.formats.ShTest(execute_external=True)
