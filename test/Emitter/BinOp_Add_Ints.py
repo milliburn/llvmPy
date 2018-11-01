fff11b7fe7dbea7f666b1e8cd3c20e6b08a1bf9a
Roberth Kulbin <roberth@winter-weht.net>
2018-11-01 17:02:32 +0000

    Port more tests.

---
+++ test/Emitter/BinOp_Add_Ints.py
@@ -0,0 +1,14 @@
+# RUN: llvmPy --ir %s > %t1
+# RUN: cat -n %t1 >&2
+# RUN: llvm-as < %t1 | llvm-dis | FileCheck %s
+
+1 + 2
+
+# CHECK: define
+# CHECK-SAME: @__body__
+
+# CHECK: [[LHS:%[0-9]]] = call %PyObj* @llvmPy_int(i64 1)
+# CHECK-NEXT: [[RHS:%[0-9]]] = call %PyObj* @llvmPy_int(i64 2)
+# CHECK-NEXT: [[RV:%[0-9]]] = call %PyObj* @llvmPy_add(%PyObj* [[LHS]], %PyObj* [[RHS]])
+
+# CHECK-DAG: declare %PyObj* @llvmPy_add(%PyObj*, %PyObj*)
