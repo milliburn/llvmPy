970c771ae5ce7c49c1c457f1cb7af9a51a85adf0
Roberth Kulbin <roberth@winter-weht.net>
2018-11-03 13:33:08 +0000

    Fix PATH for lit popen.

--- test/TestSuite.cc
+++ test/TestSuite.cc
@@ -2,14 +2,22 @@
 #include <llvmPy/LitParser.h>
 #include <catch2/catch.hpp>
 #include <llvmPyTest.h>
+#include <stdlib.h>
 using namespace llvmPy;
 
 TEST_CASE("Test Suite") {
+    std::string PATH(getenv("PATH"));
+    PATH = std::string(BINARY_DIR) +
+            ":" +
+            std::string(LLVM_DIR) +
+            "/../../../bin:" +
+            PATH;
+    setenv("PATH", PATH.c_str(), 1);
     std::vector<LitTestResult *> results;
     lit(results, TEST_PATH);
 
     for (auto result : results) {
-        DYNAMIC_SECTION(result->getTestName()) {
+        SECTION(result->getTestName()) {
             switch (result->getResultCode()) {
             case LitResultCode::PASS:
             case LitResultCode::XPASS:
