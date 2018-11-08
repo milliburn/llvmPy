7200dc5719ce634d28fbf350a9f7823e2b7a72d2
Roberth Kulbin <roberth@winter-weht.net>
2018-11-08 21:20:10 +0000

    Fix slow running time of lit tests run via Catch.
    It had to do with the test fixture resulting in an exponential re-run of all the tests.

--- test/TestSuite.cc
+++ test/TestSuite.cc
@@ -20,47 +20,60 @@ static std::vector<std::string> tokenize(
     return tokens;
 }
 
-TEST_CASE("Test Suite") {
+static std::vector<LitTestResult *> *
+runTests()
+{
     std::string PATH(getenv("PATH"));
     PATH = std::string(BINARY_DIR) +
-            ":" +
-            std::string(LLVM_DIR) +
-            "/../../../bin:" +
-            PATH;
+           ":" +
+           std::string(LLVM_DIR) +
+           "/../../../bin:" +
+           PATH;
     setenv("PATH", PATH.c_str(), 1);
-    std::vector<LitTestResult *> results;
-    lit(results, TEST_PATH);
+    auto *results = new std::vector<LitTestResult *>();
+    lit(*results, TEST_PATH);
+    return results;
+}
 
-    for (auto result : results) {
+static std::vector<LitTestResult *> *results;
+
+static void
+report(LitTestResult const &result)
+{
+    switch (result.getResultCode()) {
+    case LitResultCode::PASS:
+    case LitResultCode::XPASS:
+        SUCCEED(
+                "Test case successful (" <<
+                result.getResultCode() <<
+                ").");
+        break;
+
+    default:
+        FAIL_CHECK(
+                "Test case " <<
+                result.getSuiteName() <<
+                " :: " <<
+                result.getTestName() <<
+                " failed with status " <<
+                result.getResultCode() <<
+                " and the following output:\n\n" <<
+                result.getOutput());
+        break;
+    }
+}
+
+TEST_CASE("Test Suite") {
+    if (!results) {
+        results = runTests();
+    }
+
+    for (auto result : *results) {
         std::vector<std::string> sections =
                 tokenize(result->getTestName(), '/');
 
-        REQUIRE(sections.size() == 2);
-
         SECTION(result->getTestName()) {
-            // SECTION(sections[1]) {
-                switch (result->getResultCode()) {
-                case LitResultCode::PASS:
-                case LitResultCode::XPASS:
-                    SUCCEED(
-                            "Test case successful (" <<
-                            result->getResultCode() <<
-                            ").");
-                    break;
-
-                default:
-                    FAIL_CHECK(
-                            "Test case " <<
-                            result->getSuiteName() <<
-                             " :: " <<
-                             result->getTestName() <<
-                             " failed with status " <<
-                             result->getResultCode() <<
-                             " and the following output:\n\n" <<
-                             result->getOutput());
-                    break;
-                }
-            // }
+            report(*result);
         }
     }
 }
