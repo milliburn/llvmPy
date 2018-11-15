e765e42690b4c62daf8276328dfcded4f7fc9fef
Roberth Kulbin <roberth@winter-weht.net>
2018-11-15 18:06:26 +0000

    Introduce new casting operators.

--- include/llvmPy/Typed.h
+++ include/llvmPy/Typed.h
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <assert.h>
+
 #ifdef __cplusplus
 namespace llvmPy {
 
 template<typename T>
 class Typed {
 public:
+    // Ensure the class is polymorphic.
+    virtual ~Typed() = default;
+
     static bool classof(Typed const *x) {
         return false;
     }
@@ -32,6 +37,40 @@ public:
 
     bool isType(T t) const {
         return type == t;
+    }
+
+    template<class K>
+    bool isa() {
+        return !!this->cast<K>();
+    }
+
+    template<class K>
+    bool isa() const {
+        return !!this->cast<K const>();
+    }
+
+    template<class K>
+    K *cast() {
+        return dynamic_cast<K *>(this);
+    }
+
+    template<class K>
+    K const *cast() const {
+        return dynamic_cast<K const *>(this);
+    }
+
+    template<class K>
+    K &as() {
+        auto *ptr = this->cast<K>();
+        assert(ptr);
+        return *ptr;
+    }
+
+    template<class K>
+    K const &as() const {
+        auto *ptr = this->cast<K const>();
+        assert(ptr);
+        return *ptr;
     }
 
 protected:
