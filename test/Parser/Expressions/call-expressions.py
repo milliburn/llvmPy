7bb2d7c11efe844a7a19ea128bda643af4753234
Roberth Kulbin <roberth@winter-weht.net>
2018-11-16 16:00:45 +0000

    Re-factor the Parser2 readExpr() with the explicit states in mind.
    This is to fix issues related to the precedence of function calls.

--- test/Parser/Expressions/call-expressions.py
+++ test/Parser/Expressions/call-expressions.py
@@ -2,7 +2,7 @@
 
 f(x + 1)                # CHECK:f((x + 1i))
 f(x + 2 + 3)            # CHECK-NEXT:f(((x + 2i) + 3i))
-f(x + 1) + f(x + 2)     # CHECK-NEXT:(f((x + 1i)) + f((x + 2i))
+f(x + 1) + f(x + 2)     # CHECK-NEXT:(f((x + 1i)) + f((x + 2i)))
 1 + f(x + 1)            # CHECK-NEXT:(1i + f((x + 1i)))
 f(x + 1) + 1            # CHECK-NEXT:(f((x + 1i)) + 1i)
 
