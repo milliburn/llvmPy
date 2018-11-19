7c6a385d71208a79a84fb64847cf4bea80ee8d3b
Roberth Kulbin <roberth@winter-weht.net>
2018-11-19 23:02:38 +0000

    Add test for frame pointer invalidation in assign statements.

---
+++ test/Syntax/Statements/assign/func-instr-invalidates-frame-ptr.py
@@ -0,0 +1,20 @@
+# RUN: test-ir.sh %s
+
+
+# Test that the assign statement reloads the frame pointer after a call to
+# llvmPy_func(), which may lift the frame from the stack to the heap.
+
+
+# IR-LABEL: define %PyObj* @__body__
+# IR: alloca [[F:%Frame.__body__]]
+
+# IR: [[framePtrPtr:%[^ ]+]] = getelementptr [[F]], [[F]]* %frame, i64 0, i32 0
+
+f = lambda: 1
+
+# IR: [[PyFunc:%[^ ]+]] = call %PyObj* @llvmPy_func
+# IR-NEXT: [[framePtr:%[^ ]+]] = load [[F]]*, [[F]]** [[framePtrPtr]]
+# IR-NEXT: [[fPtr:%[^ ]+]] = getelementptr [[F]], [[F]]* [[framePtr]], i64 0, i32 2, i64 0
+# IR-NEXT: store %PyObj* [[PyFunc]], %PyObj** [[fPtr]]
+
+# IR-LABEL: define %PyObj* @lambda
