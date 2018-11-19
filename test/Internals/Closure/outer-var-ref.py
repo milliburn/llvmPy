6d61d9245678d96769aefc63a5646982b5501471
Roberth Kulbin <roberth@winter-weht.net>
2018-11-19 23:32:43 +0000

    Add/replace test for outer scope variable reference.

---
+++ test/Internals/Closure/outer-var-ref.py
@@ -0,0 +1,18 @@
+# RUN: test-ir.sh %s
+
+# IR-LABEL: define %PyObj* @__body__
+# IR: alloca [[F:%Frame.__body__]]
+
+x = 1
+
+# IR: [[xPtr:%[^ ]+]] = getelementptr [[F]], [[F]]* {{%[^ ]+}}, i64 0, i32 2, i64 [[xIdx:[0-9]+]]
+# IR: store %PyObj* %PyInt.1, %PyObj** [[xPtr]]
+
+f = lambda: x + 2
+
+# IR-LABEL: define %PyObj* @lambda
+# IR: load %Frame.lambda
+# IR: [[outerPtr:%[^ ]+]] = load [[F]]*, [[F]]** {{%[^ ]+}}
+# IR-NEXT: [[xPtr:%[^ ]+]] = getelementptr [[F]], [[F]]* [[outerPtr]], i64 0, i32 2, i64 [[xIdx]]
+# IR-NEXT: [[x:%[^ ]+]] = load %PyObj*, %PyObj** [[xPtr]]
+# IR: call %PyObj* @llvmPy_add(%PyObj* [[x]]
