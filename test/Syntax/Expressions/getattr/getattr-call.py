9a4fc20abffc9fd3aa46560ef294ee1b6e651b52
Roberth Kulbin <roberth@winter-weht.net>
2018-11-21 20:47:00 +0000

    Adjust getattr-call test to match.

--- test/Syntax/Expressions/getattr/getattr-call.py
+++ test/Syntax/Expressions/getattr/getattr-call.py
@@ -9,16 +9,18 @@ print("test".upper().capitalize())
 
 # PARSER:print("test".upper().capitalize())
 
-# IR-DAG: @name.upper = private constant i8*
+# IR-DAG: @name.upper = private unnamed_addr constant [6 x i8] c"upper\00"
+# IR-DAG: @name.capitalize = private unnamed_addr constant [11 x i8] c"capitalize\00"
 # IR-LABEL: @__body__
 # IR: [[str:%[^ ]+]] = load %PyObj*, %PyObj** @PyStr.0
-# IR-NEXT: [[upper:%[^ ]+]] = call %PyObj* getattr(%PyObj* [[str]], i8* @name.upper)
-# IR-NEXT: %callframe = alloca %Frame*
-# IR-NEXT: [[upperFunc:%[^ ]+]] = call i8* @llvmPy_fchk(%Frame** %callframe, %PyObj* [[upper]], i64 1)
-# IR-NEXT: [[upperVal:%[^ ]+]] = call %PyObj* [[upperFunc]](%Frame** %callframe, %PyObj* [[str]])
-# IR-NEXT: [[capitalize:%[^ ]+]] = call %PyObj* getattr(%PyObj* [[upperVal]], i8* @name.capitalize)
-# IR-NEXT: [[capitalizeFunc:%[^ ]+]] = call i8* @llvmPy_fchk(%Frame** %callframe, %PyObj* [[upperVal]], i64 1)
-# IR-NEXT: [[capitalizeVal:%[^ ]+]] = call [[capitalizeFunc]](%Frame** %callframe, %PyObj* [[upperVal]])
-# IR-NEXT: @llvmPy_print(%PyObj* [[capitalizeVal]])
+# IR: [[upper:%[^ ]+]] = call %PyObj* @llvmPy_getattr(%PyObj* [[str]], i8* getelementptr inbounds ([6 x i8], [6 x i8]* @name.upper, i32 0, i32 0))
+# IR: [[upperFunc_:%[^ ]+]] = call i8* @llvmPy_fchk(%Frame** %callframe, %PyObj* [[upper]], i64 1)
+# IR: [[upperFunc:%[^ ]+]] = bitcast i8* [[upperFunc_]] to %PyObj* (%Frame**, %PyObj*)*
+# IR: [[upperVal:%[^ ]+]] = call %PyObj* [[upperFunc]](%Frame** %callframe, %PyObj* [[str]])
+# IR: [[capitalize:%[^ ]+]] = call %PyObj* @llvmPy_getattr(%PyObj* [[upperVal]], i8* getelementptr inbounds ([11 x i8], [11 x i8]* @name.capitalize, i32 0, i32 0))
+# IR: [[capitalizeFunc_:%[^ ]+]] = call i8* @llvmPy_fchk(%Frame** %callframe1, %PyObj* [[capitalize]], i64 1)
+# IR: [[capitalizeFunc:%[^ ]+]] = bitcast i8* [[capitalizeFunc_]] to %PyObj* (%Frame**, %PyObj*)*
+# IR: [[capitalizeVal:%[^ ]+]] = call %PyObj* [[capitalizeFunc]](%Frame** %callframe1, %PyObj* [[upperVal]])
+# IR: @llvmPy_print(%PyObj* [[capitalizeVal]])
 
 # OUTPUT: "Test"
