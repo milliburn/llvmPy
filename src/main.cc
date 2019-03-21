f975b9ecc0ff9c266ec97163be8ebbc41f8c48f2
Roberth Kulbin <roberth@winter-weht.net>
2019-03-21 23:00:21 +0000

    Revert "Add Boost::program_options."
    This reverts commit 9832cd23e7abdc28d2913ea30e1ca28caceffb73.

--- src/main.cc
+++ src/main.cc
@@ -6,21 +6,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weverything"
-#pragma GCC diagnostic ignored "-Wpedantic"
-#include <boost/program_options.hpp>
-#pragma GCC diagnostic pop
-
 using namespace llvmPy;
-namespace po = boost::program_options;
 namespace cl = llvm::cl;
 using std::cerr;
 using std::cout;
 using std::endl;
 using std::string;
-using std::vector;
 
 static cl::opt<string> Cmd(
         "c",
@@ -61,7 +52,6 @@ struct Options {
     Stage stage;
     Mode mode;
     string program;
-    vector<string> impl;
 };
 
 static int run(Options const &);
@@ -74,48 +64,30 @@ main(int argc, char **argv)
             .mode = Mode::UNKNOWN,
     };
 
-    po::options_description desc("Options");
-    desc.add_options()
-            ("help,h,?", "Show help.")
-            ("version,v", "Show program version.")
-            ("X", po::value<vector<string>>(), "Set implementation-specific option.")
-            ("c", po::value<string>(&options.program), "Run program passed as string");
+    cl::ParseCommandLineOptions(argc, argv);
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+    if (IsLexer) {
+        options.stage = Stage::LEXER;
+    } else if (IsParser) {
+        options.stage = Stage::PARSER;
+    } else if (IsIR) {
+        options.stage = Stage::IR;
+    }
 
-    if (vm.count("help")) {
-        cout << desc << endl;
-        return 0;
+    if (Cmd.getPosition() && Filename.getPosition()) {
+        cerr << "Only one of cmd or filename may be specified." << endl;
+        exit(1);
+    } else if (Cmd.getPosition()) {
+        options.mode = Mode::COMMAND;
+        options.program = Cmd.getValue();
+    } else if (Filename.getPosition()) {
+        options.mode = Mode::SCRIPT;
+        options.program = Filename.getValue();
+    } else {
+        options.mode = Mode::STDIN;
     }
 
     return run(options);
-
-//     cl::ParseCommandLineOptions(argc, argv);
-//
-//     if (IsLexer) {
-//         options.stage = Stage::LEXER;
-//     } else if (IsParser) {
-//         options.stage = Stage::PARSER;
-//     } else if (IsIR) {
-//         options.stage = Stage::IR;
-//     }
-//
-//     if (Cmd.getPosition() && Filename.getPosition()) {
-//         cerr << "Only one of cmd or filename may be specified." << endl;
-//         exit(1);
-//     } else if (Cmd.getPosition()) {
-//         options.mode = Mode::COMMAND;
-//         options.program = Cmd.getValue();
-//     } else if (Filename.getPosition()) {
-//         options.mode = Mode::SCRIPT;
-//         options.program = Filename.getValue();
-//     } else {
-//         options.mode = Mode::STDIN;
-//     }
-//
-//     return run(options);
 }
 
 static int
