d69c84f2133e80ebd70ac775fcfcd4da5adb793a
Roberth Kulbin <roberth@winter-weht.net>
2019-03-22 18:25:03 +0000

    Implement the majority of the AST dispatch in AstPass.h.

--- include/llvmPy/Compiler/Parser/AstPass.h
+++ include/llvmPy/Compiler/Parser/AstPass.h
@@ -15,9 +15,19 @@ protected:
 
     virtual void updateExpr(Expr &expr);
     virtual void updateLambdaExpr(LambdaExpr &lambda);
+    virtual void updateBinaryExpr(BinaryExpr &binary);
+    virtual void updateCallExpr(CallExpr &call);
+    virtual void updateTupleExpr(TupleExpr &tuple);
+    virtual void updateGetattrExpr(GetattrExpr &getattr);
 
     virtual void updateStmt(Stmt &stmt);
     virtual void updateExprStmt(ExprStmt &stmt);
+    virtual void updateAssignStmt(AssignStmt &assign);
+    virtual void updateCompoundStmt(CompoundStmt &compound);
+    virtual void updateDefStmt(DefStmt &def);
+    virtual void updateReturnStmt(ReturnStmt &ret);
+    virtual void updateConditionalStmt(ConditionalStmt &cond);
+    virtual void updateWhileStmt(WhileStmt &loop);
 
     template<class T>
     T &getParentOfType(AST &ast) const {
--- src/Compiler/Parser/AstPass.cc
+++ src/Compiler/Parser/AstPass.cc
@@ -30,12 +30,52 @@ AstPass::updateExpr(Expr &expr)
 {
     if (auto *lambda = expr.cast<LambdaExpr>()) {
         updateLambdaExpr(*lambda);
+    } else if (auto *binary = expr.cast<BinaryExpr>()) {
+        updateBinaryExpr(*binary);
+    } else if (auto *call = expr.cast<CallExpr>()) {
+        updateCallExpr(*call);
+    } else if (auto *tuple = expr.cast<TupleExpr>()) {
+        updateTupleExpr(*tuple);
+    } else if (auto *getattr = expr.cast<GetattrExpr>()) {
+        updateGetattrExpr(*getattr);
     }
 }
 
 void
 AstPass::updateLambdaExpr(LambdaExpr &expr)
 {
+    updateExpr(expr.getExpr());
+}
+
+void
+AstPass::updateBinaryExpr(BinaryExpr &binary)
+{
+    updateExpr(binary.getLeftOperand());
+    updateExpr(binary.getRightOperand());
+}
+
+void
+AstPass::updateCallExpr(CallExpr &call)
+{
+    updateExpr(call.getCallee());
+
+    for (auto &arg : call.args()) {
+        updateExpr(arg);
+    }
+}
+
+void
+AstPass::updateTupleExpr(TupleExpr &tuple)
+{
+    for (auto &member : tuple.members()) {
+        updateExpr(member);
+    }
+}
+
+void
+AstPass::updateGetattrExpr(GetattrExpr &getattr)
+{
+    updateExpr(getattr.getObject());
 }
 
 void
@@ -43,10 +83,64 @@ AstPass::updateStmt(Stmt &stmt)
 {
     if (auto *expr = stmt.cast<ExprStmt>()) {
         updateExprStmt(*expr);
+    } else if (auto *assign = stmt.cast<AssignStmt>()) {
+        updateAssignStmt(*assign);
+    } else if (auto *compound = stmt.cast<CompoundStmt>()) {
+        updateCompoundStmt(*compound);
+    } else if (auto *def = stmt.cast<DefStmt>()) {
+        updateDefStmt(*def);
+    } else if (auto *ret = stmt.cast<ReturnStmt>()) {
+        updateReturnStmt(*ret);
+    } else if (auto *cond = stmt.cast<ConditionalStmt>()) {
+        updateConditionalStmt(*cond);
+    } else if (auto *loop = stmt.cast<WhileStmt>()) {
+        updateWhileStmt(*loop);
     }
 }
 
 void
 AstPass::updateExprStmt(ExprStmt &stmt)
 {
+    updateExpr(stmt.getExpr());
+}
+
+void
+AstPass::updateAssignStmt(AssignStmt &assign)
+{
+    updateExpr(assign.getValue());
+}
+
+void
+AstPass::updateCompoundStmt(CompoundStmt &compound)
+{
+    for (auto &stmt : compound.statements()) {
+        updateStmt(stmt);
+    }
+}
+
+void
+AstPass::updateDefStmt(DefStmt &def)
+{
+    updateStmt(def.getBody());
+}
+
+void
+AstPass::updateReturnStmt(ReturnStmt &ret)
+{
+    updateExpr(ret.getExpr());
+}
+
+void
+AstPass::updateConditionalStmt(ConditionalStmt &cond)
+{
+    updateExpr(cond.getCondition());
+    updateStmt(cond.getThenBranch());
+    updateStmt(cond.getElseBranch());
+}
+
+void
+AstPass::updateWhileStmt(WhileStmt &loop)
+{
+    updateExpr(loop.getCondition());
+    updateStmt(loop.getBody());
 }
