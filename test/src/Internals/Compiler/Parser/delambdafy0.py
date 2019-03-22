87ee8b86f9b7892e6ff1c00a8a6c6eae9bbb8cea
Roberth Kulbin <roberth@winter-weht.net>
2019-03-22 21:01:24 +0000

    Extends the delambdafy test.

--- test/src/Internals/Compiler/Parser/delambdafy0.py
+++ test/src/Internals/Compiler/Parser/delambdafy0.py
@@ -1,8 +1,9 @@
 # RUN: test-parser.sh %s delambdafy
+# RUN: test-output.sh %s
 
 print("Start")
-
 # PARSER-LABEL:print("Start")
+# OUTPUT-LABEL: Start
 
 first = lambda: 1
 
@@ -10,17 +11,48 @@ first = lambda: 1
 # PARSER-NEXT:    return 1i
 # PARSER-NEXT:first = __lambda__0
 
+print(str(first()))
+# PARSER-NEXT:print(str(first()))
+# OUTPUT-NEXT: 1
+
 print("Middle")
 # PARSER-NEXT:print("Middle")
+# OUTPUT-NEXT: Middle
 
 def func(y):
     second = 1 + (lambda x: x + 2)(y)
+    return second
 
 # PARSER-NEXT:def func(y):
 # PARSER-NEXT:    def __lambda__1(x):
 # PARSER-NEXT:        return (x + 2i)
 # PARSER-NEXT:    second = (1i + __lambda__1(y))
+# PARSER-NEXT:    return second
+
+print(str(func(10)))
+# PARSER-NEXT:print(str(func(10i)))
+# OUTPUT-NEXT: 13
+
+print("Third")
+# PARSER-NEXT:print("Third")
+# OUTPUT-NEXT: Third
+
+def cnuf(y):
+    third = (lambda x: x + 2)(y) + (lambda z: z + 3)(y) + first()
+    return third
+
+# PARSER-NEXT:def cnuf(y):
+# PARSER-NEXT:    def __lambda__2(x):
+# PARSER-NEXT:        return (x + 2i)
+# PARSER-NEXT:    def __lambda__3(z):
+# PARSER-NEXT:        return (z + 3i)
+# PARSER-NEXT:    third = ((__lambda__2(y) + __lambda__3(y)) + first())
+# PARSER-NEXT:    return third
+
+print(str(cnuf(10)))
+# PARSER-NEXT:print(str(cnuf(10i)))
+# OUTPUT-NEXT: 26
 
 print("End")
-
 # PARSER-NEXT:print("End")
+# OUTPUT-NEXT: End
